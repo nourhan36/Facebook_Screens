c30b720a31c19ef36688c90cf746ee0f6851842d
nourhan36 <nourhanibrahim2004@gmail.com>
2024-01-02 22:04:04 +0200

    Initial commit

--- app/src/main/java/com/example/facebook_screens/SplashScreen.kt
+++ app/src/main/java/com/example/facebook_screens/SplashScreen.kt
@@ -8,7 +8,8 @@ import androidx.appcompat.app.AppCompatActivity
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.acticity_splash_screen)
+        setContentView(R.layout.activity_splash_screen)
+
 
         val intent = Intent(this, MainPage::class.java)
 
